31add8c63a80a54e3e06382f42eda5cddd3d5f07
minku-koo <koomk97@naver.com>
2023-01-20 16:43:09 +0900

    Update pyFeatureCompare.py

--- pyFeatureCompare.py
+++ pyFeatureCompare.py
@@ -10,8 +10,8 @@ import tkinter as tk
 # import difflib
 
 class KoreanDiff:
-    def __init__(self, source_path, compare_col):
-        # xls -> xlsx
+    def __init__(self, source_path, compare_col, diffYes_col):
+        # xls -> xlsx (xls 파일은 작업 불가능함)
         if source_path.split(".")[-1] == 'xls':
             temp = source_path
             source_path += 'x'
@@ -19,7 +19,8 @@ class KoreanDiff:
             df.to_excel(source_path, index=False)
 
         self.source_excel_path = source_path
-        splitChar = '//'
+        splitChar = '//' 
+        # 파일 경로 일치시킴
         if '\\' not in source_path: 
             splitChar = '/'
 
@@ -29,13 +30,14 @@ class KoreanDiff:
 
         # compare column number
         self.source_row_char, self.target_row_char = compare_col
+        self.diffYesCol = diffYes_col
 
         # compare column char
         self.source_row_num = ord(self.source_row_char) - 65
         self.target_row_num = ord(self.target_row_char) - 65
+        self.diff_yes_num   = ord(self.diffYesCol) - 65
 
-        # self.compare_col = compare_col
-
+        # create workbook
         self.output_workbook = xlsxwriter.Workbook(self.target_excel_path)
         self.output_sheet = self.output_workbook.add_worksheet()
 
@@ -44,8 +46,8 @@ class KoreanDiff:
         self.output_sheet.set_column(self.source_row_num, self.source_row_num, self.cell_width)
         self.output_sheet.set_column(self.target_row_num, self.target_row_num, self.cell_width)
 
+        # cell background color setting
         self.change_bg_color = '#FFFFCC'
-        self.title_bg_color = ''
         self.head_bg_color = '#FF6600'
 
         self.rem_word = '_x000D_'
@@ -53,10 +55,11 @@ class KoreanDiff:
         # text auto newline
         self.cell_format = self.output_workbook.add_format({'text_wrap': True, 'bg_color': self.change_bg_color, 'border': 1})
         self.cell_head = self.output_workbook.add_format({'text_wrap': True, 'bg_color': self.head_bg_color, 'border': 1})
-        # red/black text color
+        # text color
         self.red_color = self.output_workbook.add_format({'color': 'red', 'text_wrap': True, 'bg_color': self.change_bg_color, 'border': 1})
         self.black_color = self.output_workbook.add_format({'color': 'black', 'text_wrap': True, 'bg_color': self.change_bg_color, 'border': 1})
-        
+        self.orange_color = self.output_workbook.add_format({'color': 'orange', 'text_wrap': True, 'bg_color': self.change_bg_color, 'border': 1})
+        self.green_color = self.output_workbook.add_format({'color': 'green', 'text_wrap': True, 'bg_color': self.change_bg_color, 'border': 1})
         return
 
     def write_data_to_cell(self, pos, data, start, size):
@@ -74,8 +77,6 @@ class KoreanDiff:
         start_with = 'black'
         loc = pos[0] + str(pos[1])  # cell location
 
-        
-        
         for i, point in enumerate(start):
             if i == 0:  # if first data
                 if point > 0: #start with red
@@ -118,6 +119,7 @@ class KoreanDiff:
 
         # do  get matching blocks
         for r_index, row in enumerate(self.wb[self.first_sheetname].iter_rows(min_row = 0)):
+            # 피쳐명인 경우
             if not row[0].value and row[self.source_row_num].value:
                 self.output_sheet.write(self.source_row_char + str(r_index + 1), row[self.source_row_num].value, self.cell_head)
                 continue
@@ -134,10 +136,26 @@ class KoreanDiff:
                     row[self.source_row_num].value = row[self.source_row_num].value.replace(self.rem_word, '')
                     self.output_sheet.write(self.source_row_char + str(r_index + 1), row[self.source_row_num].value, self.red_color)
                 continue
+            
 
-            # same match -> black
+            # remove trash word
             row[self.source_row_num].value = row[self.source_row_num].value.replace(self.rem_word, '')
             row[self.target_row_num].value = row[self.target_row_num].value.replace(self.rem_word, '')
+            
+            # 둘이 같은데 y 인 경우 -> continue
+            # 둘이 같은데 모델 확인해야하는 경우 -> continue
+            if row[self.source_row_num].value == row[self.target_row_num].value:
+                font_color = self.black_color
+                if '[모델사양' in row[self.target_row_num].value: # 모델사양 확인
+                    font_color = self.orange_color
+                elif row[self.diff_yes_num].value == "Y": # 첨부파일의 변경
+                    font_color = self.green_color
+                else: # 변경점 없음
+                    pass
+
+                self.output_sheet.write(self.source_row_char + str(r_index + 1), row[self.source_row_num].value, font_color)
+                self.output_sheet.write(self.target_row_char + str(r_index + 1), row[self.target_row_num].value, font_color)
+                continue
             
             s = SequenceMatcher(None, row[self.source_row_num].value, row[self.target_row_num].value)
             source_index_list, target_index_list, size_list = [], [], []
@@ -158,23 +176,24 @@ class FeatureCompare:
         self.source_dir_path = "[Difference Chart Directory Path]"
         self.errorMsg = ""
         self.processPercent = 0
-        self.compare_column = ['B', 'G']
+        self.compare_column = ['B', 'G', 'K']
 
-        title = "Feature Compare"
-        subtitle = 'DA Feature Compare'
+        title = "Feature Compare v1.0"
+        subtitle = 'PRMS Feature Compare'
         comment = """
-1. Open -> Feature Compare File 선택
+1. Open -> PRMS에서 Export한 Feature Compare File 선택
 2. Feature Compare에서 비교하고 싶은 두 Column을 입력 (기본값은 B, G)
-3. Compare 버튼을 통해 Feature compare 실행
-4. 선택한 Feature Compare와 같은 폴더에 compared_ 로 시작하는 결과 파일이 생성됩니다.
+3. 세번째 칸에 Y (변경됨)가 적힌 Column을 입력 (기본값은 K)
+4. Compare 버튼을 통해 실행
+5. 작업 문서와 동일한 폴더에 'compared_' 로 시작하는 결과 파일이 생성됩니다.
 
-(※ 작업 폴더에 동일한 이름의 xlsx 파일이 생성될 수 있습니다.)
+(※ 작업 폴더에 동일한 이름의 xlsx 파일이 생성될 수 있습니다)
         """
-        copyrighter = "문의 및 VOC : minku.koo"
+        copyrighter = "문의 : @minku.koo"
         
         self.window = tk.Tk()
         self.window.title(title)
-        self.window.geometry('600x500')
+        self.window.geometry('600x600')
 
         subtitleFont = tkinter.font.Font(size = 18, weight = 'bold')
         entryFont = tkinter.font.Font(size = 13)
@@ -230,7 +249,15 @@ class FeatureCompare:
                                         font=entryFont
                                     )
         self.compareColEntryB.insert(0, self.compare_column[1])
-        self.compareColEntryB.pack(pady=12)
+        self.compareColEntryB.pack(pady=6)
+
+        self.diffYesCol = tkinter.Entry(
+                                        self.window,
+                                        width=6,
+                                        font=entryFont
+                                    )
+        self.diffYesCol.insert(0, self.compare_column[2])
+        self.diffYesCol.pack(pady=12)
         
         btnBorderFrame = tkinter.LabelFrame(self.window, bd = 2, bg = "black")
         btnBorderFrame.pack()
@@ -264,6 +291,9 @@ class FeatureCompare:
         return 
 
     def __fileOpen(self):
+        '''
+        작업 파일 Open
+        '''
         filepath = fd.askopenfilename(filetypes=[("Excel file","*.xlsx"),("Excel file", "*.xls")])
 
         if filepath:
@@ -274,16 +304,24 @@ class FeatureCompare:
         return 
 
     def __compareText(self):
-        k_diff = KoreanDiff(self.source_dir_path, (self.compareColEntryA.get(), self.compareColEntryB.get()))
+        '''
+        - Compare 버튼을 누르면 동작하는 event 함수
+        '''
+        k_diff = KoreanDiff(self.source_dir_path, (self.compareColEntryA.get(), self.compareColEntryB.get()), self.diffYesCol.get())
         check = False
 
         try:
-            check = k_diff.run()
+            check = k_diff.run() # 컴페어 실행
         except IndexError as e:
+            # 컴페어 실패할 경우 (컬럼에 데이터가 없는 경우)
             print(e)
             self.progressBar.config(text = "Error: Column data is not in Excel file !!", fg = 'red')
+        
+        except:
+            self.progressBar.config(text = "Error !!", fg = 'red')
 
         if check:
+            # 실행 성공
             self.progressBar.config(text = "Feature Compare Done", fg = 'blue')
 
         return 
